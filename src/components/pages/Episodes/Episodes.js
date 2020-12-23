27243e77d2271a1411a98c1c94feb830d770bd2c
Richard Shu <richardjshu@gmail.com>
2020-12-22 17:03:05 -0800

    Add Episodes page with guest cards

--- src/components/pages/Episodes/Episodes.js
+++ src/components/pages/Episodes/Episodes.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-import { Container } from "react-bootstrap";
+import { Card, Container } from "react-bootstrap";
+import { EpisodesData } from "../../../data/episodes.js";
 import "./Episodes.css";
 
 class Episodes extends Component {
@@ -7,6 +8,28 @@ class Episodes extends Component {
     return (
       <Container className="Episodes">
         <h1 className="page-title">Episodes</h1>
+        <div className="card-container">
+          {EpisodesData.map((episode, key) => {
+            return (
+              <Card key={key} style={{ width: "18rem" }}>
+                <Card.Img
+                  variant="top"
+                  src={
+                    require(`../../../images/guests/${episode.id}.jpg`).default
+                  }
+                  alt={episode.guest}
+                />
+                <Card.Body>
+                  <Card.Title>
+                    {episode.title} -
+                    <span className="guest-name"> {episode.guest}</span>
+                  </Card.Title>
+                  <Card.Text>{episode.description}</Card.Text>
+                </Card.Body>
+              </Card>
+            );
+          })}
+        </div>
       </Container>
     );
   }
---
+++ src/data/episodes.js
@@ -0,0 +1,196 @@
+export const EpisodesData = [
+  {
+    id: "ep20",
+    guest: "Rebekah Hwang",
+    title: "The Reality of Being a Teacher",
+    description: "Being a teacher is hard but rewarding.",
+    date: "December 2, 2020",
+    anchor_url:
+      "https://anchor.fm/itsover/embed/episodes/Ep-20---The-Reality-of-Being-a-Teacher---Rebekah-Ramirez-en8caa",
+  },
+  {
+    id: "ep19",
+    guest: "Yehong Zhu",
+    title: "Building a Tech Startup (Zette)",
+    description:
+      "On the highs and lows of entrepreneurship and taking the leap of faith into startup land. Exciting stuff to come at zette.media.",
+    date: "November 25, 2020",
+    anchor_url:
+      "https://anchor.fm/itsover/embed/episodes/Ep-19---Building-a-Tech-Startup-Zette---Yehong-Zhu-emtqg3",
+  },
+  {
+    id: "ep18",
+    guest: "Andrew Chou",
+    title: "Running for Diamond Bar City Council",
+    description:
+      "How city council works and what it’s like to campaign for office.",
+    date: "November 11, 2020",
+    anchor_url:
+      "https://anchor.fm/itsover/embed/episodes/Ep-18---Running-for-Diamond-Bar-City-Council---Andrew-Chou-emag1i",
+  },
+  {
+    id: "ep17",
+    guest: "Tanisha Basrai",
+    title: "How to Win Friends and Influence People",
+    description: "Humor is important and how to (politely) exit conversations.",
+    date: "November 4, 2020",
+    anchor_url:
+      "https://anchor.fm/itsover/embed/episodes/Ep-17---How-to-Win-Friends-and-Influence-People---Tanisha-Basrai-em0mji",
+  },
+  {
+    id: "ep16",
+    guest: "Karlie Chen",
+    title: "Starting a YouTube Channel During a Pandemic",
+    description:
+      "Vlogging is hard (and so is editing videos). Check out what it's like at Cornell on Karlie's channel: karlietchen.",
+    date: "October 21, 2020",
+    anchor_url:
+      "https://anchor.fm/itsover/embed/episodes/Ep-16---Starting-a-YouTube-Channel-During-a-Pandemic---Karlie-Chen-el7ctm",
+  },
+  {
+    id: "ep15",
+    guest: "Amy Miyahara",
+    title: "Undeclared at Stanford",
+    description:
+      "Choosing a career path when you have too many (or too few) options.",
+    date: "October 14, 2020",
+    anchor_url:
+      "https://anchor.fm/itsover/embed/episodes/Ep-15---Undeclared-at-Stanford---Amy-Miyahara-el13gr",
+  },
+  {
+    id: "ep14",
+    guest: "Stephanie Del Paggio",
+    title: "Interviewing New Yorkers for 52 Stories (a NYC blog)",
+    description:
+      "How interviewing New Yorkers in the streets, coffee shops, and underground subway turned into a massive blog. Read about their stories at 52storiesnyc.com.",
+    date: "October 7, 2020",
+    anchor_url:
+      "https://anchor.fm/itsover/embed/episodes/My-Grand-Project-52-Stories-a-NYC-blog-ekn9sd",
+  },
+  {
+    id: "ep13",
+    guest: "Noble San",
+    title: "A Friendship That Fell Apart",
+    description: "Reuniting with a friend after 6 years of silence.",
+    date: "September 30, 2020",
+    anchor_url:
+      "https://anchor.fm/itsover/embed/episodes/Ep-13---A-Friendship-That-Fell-Apart---Noble-San-ekctuo",
+  },
+  {
+    id: "ep12",
+    guest: "Emma Quong",
+    title: "Studying Abroad in 5 Countries",
+    description:
+      "On being whisked away to New York, Florence, Shanghai, Prague, and Madrid.",
+    date: "September 23, 2020",
+    anchor_url:
+      "https://anchor.fm/itsover/embed/episodes/Ep-12---Studying-Abroad-in-5-Countries---Emma-Quong-ek1ubk",
+  },
+  {
+    id: "ep11",
+    guest: "Cathy Xu",
+    title: "Being Asian with a British Accent",
+    description:
+      "Assimilating into white culture and getting the best (and worst) of both worlds.",
+    date: "September 16, 2020",
+    anchor_url:
+      "https://anchor.fm/itsover/embed/episodes/Ep-11---Being-Asian-with-a-British-Accent---Cathy-Xu-ejncss",
+  },
+  {
+    id: "ep10",
+    guest: "Jeevani Midigaspe",
+    title: "Behind the Scenes of HR",
+    description: "How to stand out when applying to jobs and other HR secrets.",
+    date: "September 9, 2020",
+    anchor_url:
+      "https://anchor.fm/itsover/embed/episodes/Ep-10---Behind-the-Scenes-of-HR---Jeevani-Midigaspe-ejbgon",
+  },
+  {
+    id: "ep09",
+    guest: "Kanon Mori",
+    title: "Modernizing Healthcare with AI & Government Reform",
+    description:
+      "Exploring healthcare career options in public and private sectors (WHO vs. health tech).",
+    date: "September 2, 2020",
+    anchor_url:
+      "https://anchor.fm/itsover/embed/episodes/Ep-09---Modernizing-Healthcare-with-AI--Government-Reform---Kanon-Mori-ej0e6k",
+  },
+  {
+    id: "ep08",
+    guest: "Ted Yarmoski",
+    title: "College Roommate Banana Horror Story",
+    description:
+      '"He asked me if he could borrow my blow dryer and I said no" ...and more college roommate stories.',
+    date: "August 26, 2020",
+    anchor_url:
+      "https://anchor.fm/itsover/embed/episodes/Ep-08---College-Roommate-Banana-Horror-Story---Ted-Yarmoski-eim9g5",
+  },
+  {
+    id: "ep07",
+    guest: "Hannah Hsieh",
+    title: "Life at a Top 5 Law Firm",
+    description:
+      "Ditching mechanical engineering to become a paralegal at a top international law firm.",
+    date: "August 12, 2020",
+    anchor_url:
+      "https://anchor.fm/itsover/embed/episodes/Ep-07---Life-at-a-Top-5-Law-Firm---Hannah-Hsieh-ei1m15",
+  },
+  {
+    id: "ep06",
+    guest: "Jerry Cheung",
+    title: "TED Talks Taught Me How to Pick Up Girls",
+    description:
+      "7 tips for socializing as an introvert. Never giving up and training hard after being rejected from a hip hop dance crew.",
+    date: "August 5, 2020",
+    anchor_url:
+      "https://anchor.fm/itsover/embed/episodes/Ep-06---TED-Talks-Taught-Me-How-to-Pick-Up-Girls-ehj9gp",
+  },
+  {
+    id: "ep05",
+    guest: "Sophia Bosque",
+    title: "Where I Draw Inspiration for My Love Songs",
+    description: "How to write a love song and more musical adventures.",
+    date: "July 29, 2020",
+    anchor_url:
+      "https://anchor.fm/itsover/embed/episodes/Ep-05---Where-I-Draw-Inspiration-for-My-Love-Songs---Sophia-Bosque-ehcecq",
+  },
+  {
+    id: "ep04",
+    guest: "Amelie Lee",
+    title: "8000 Instagram Followers @AsianAmericansForBLM",
+    description: "On growing an Instagram page to 8000 followers in one month.",
+    date: "July 22, 2020",
+    anchor_url:
+      "https://anchor.fm/itsover/embed/episodes/Ep-04---8000-Instagram-Followers-AsianAmericansForBLM---Amelie-Lee-eh2u4k",
+  },
+  {
+    id: "ep03",
+    guest: "Henry Kim",
+    title: "Joining (and Regretting) Greek Life",
+    description:
+      "From boarding school to Greek life. The pros and cons of artificial friendship.",
+    date: "July 15, 2020",
+    anchor_url:
+      "https://anchor.fm/itsover/embed/episodes/Ep-03---Joining-and-Regretting-Greek-Life---Henry-Kim-egpmsb",
+  },
+  {
+    id: "ep02",
+    guest: "Isaac No",
+    title: "Christianity, Politics, & Majoring in Math",
+    description:
+      "How going to college affects religious faith and finding communities away from home.",
+    date: "July 8, 2020",
+    anchor_url:
+      "https://anchor.fm/itsover/embed/episodes/Ep-02---Christianity--Politics---Majoring-in-Math---Isaac-No-egdnsm",
+  },
+  {
+    id: "ep01",
+    guest: "Brandon Tsai",
+    title: "Starting an Unofficial Rock Climbing Club",
+    description:
+      "How to start a rock climbing club and other Boy Scout adventures.",
+    date: "July 1, 2020",
+    anchor_url:
+      "https://anchor.fm/itsover/embed/episodes/Ep-01---Starting-an-Unofficial-Rock-Climbing-Club---Brandon-Tsai-egda31",
+  },
+];
